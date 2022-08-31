472a18aa378d92b5f975ff28cb5c65a8627addc3
Mateusz Baranowski <baku13681@gmail.com>
2022-08-31 17:55:31 +0200

    Add fields and commands.go

--- cmd/fields.go
+++ cmd/fields.go
@@ -4,13 +4,27 @@ type Response struct {
 	FrameCount string `json:"framecount"`
 	Error      string `json:"error"`
 	Result     []struct {
-		Anilist    string `json:"anilist"`
-		FileName   string `json:"filename"`
-		Episode    int    `json:"episode"`
-		From       int    `json:"from"`
-		To         int    `json:"to"`
-		Similariti int    `json:"smililariti"`
-		Video      string `json:"video"`
-		Image      string `json:"image"`
+		Anilist    Anilist `json:"anilist"`
+		FileName   string  `json:"filename"`
+		Episode    int     `json:"episode"`
+		From       int     `json:"from"`
+		To         int     `json:"to"`
+		Similariti int     `json:"smililariti"`
+		Video      string  `json:"video"`
+		Image      string  `json:"image"`
 	}
+}
+
+type Anilist struct {
+	ID       int    `json:"id"`
+	IDMal    int    `json:"idmal"`
+	Title    Title  `json:"title"`
+	Synonyms string `json:"synonyms"`
+	IsAdlut  bool   `json:"isadlut"`
+}
+
+type Title struct {
+	Native  string `json:"native"`
+	Romaji  string `json:"romaji"`
+	English string `json:"english"`
 }
